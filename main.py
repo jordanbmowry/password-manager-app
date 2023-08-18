0b8935d6047dad7f33a08a758d2ad853405641f0
Joran Mowry <jordan.mowry@gmail.com>
2023-08-18 15:08:44 -0400

    refactored code

--- main.py
+++ main.py
@@ -1,69 +1,65 @@
 from tkinter import *
 from tkinter import messagebox
-
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
 import random
 import string
 import pyperclip
 
 
 def generate_password():
-    # Use the built-in string library for character lists
-    letters = string.ascii_letters
-    numbers = string.digits
-    symbols = "!@#$%^&*()-_=+[]{}|;:,.<>?/~"
-
-    # Define minimum lengths and total password length
-    MIN_LETTERS = 8
-    MIN_SYMBOLS = 2
-    MIN_NUMBERS = 2
-    TOTAL_LENGTH = random.randint(16, 20)
-
-    def generate_segment(character_set, min_length):
-        """Generate a segment of the password using the given character set and minimum length."""
-        return [random.choice(character_set) for _ in range(min_length)]
-
-    # Start with the minimum required characters
-    password_list = generate_segment(letters, MIN_LETTERS)
-    password_list += generate_segment(symbols, MIN_SYMBOLS)
-    password_list += generate_segment(numbers, MIN_NUMBERS)
-
-    # Fill in the rest of the password length with random choices from all character sets
-    for _ in range(TOTAL_LENGTH - len(password_list)):
-        password_list.append(random.choice(letters + symbols + numbers))
-
-    # Shuffle and join the password list
-    random.shuffle(password_list)
-    password = "".join(password_list)
+    password = create_password()
     password_entry.insert(0, password)
     pyperclip.copy(password)
-    return password
 
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
+def create_password():
+    chars = combine_chars(
+        string.ascii_letters, string.digits, "!@#$%^&*()-_=+[]{}|;:,.<>?/~"
+    )
+    return "".join(random.choices(chars, k=random.randint(16, 20)))
+
+
+def combine_chars(*args):
+    return "".join(args)
+
+
 def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
 
+    if is_input_valid(website, password):
+        if show_confirmation(website, email, password):
+            append_to_data_file(website, email, password)
+            clear_entries()
+
+
+def is_input_valid(website, password):
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(
             title="Oops", message="Please make sure you haven't left any fields empty."
         )
-    else:
-        is_okay = messagebox.askokcancel(
-            title=website,
-            message=f"These are the details entered: \nEmain: {email} \nPassword: {password} \nIs it okay to save?",
-        )
-
-        if is_okay:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        return False
+    return True
 
 
-# ---------------------------- UI SETUP ------------------------------- #
+def show_confirmation(website, email, password):
+    return messagebox.askokcancel(
+        title=website,
+        message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it okay to save?",
+    )
+
+
+def append_to_data_file(website, email, password):
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password}\n")
+
+
+def clear_entries():
+    website_entry.delete(0, END)
+    password_entry.delete(0, END)
+
+
+# UI SETUP
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
