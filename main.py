9e1d50434846d8497374207f7ccaa5c135e5d33d
Joran Mowry <jordan.mowry@gmail.com>
2023-08-21 16:06:06 -0400

    create search functionality

--- main.py
+++ main.py
@@ -76,6 +76,25 @@ def clear_entries():
     password_entry.delete(0, END)
 
 
+def find_password():
+    try:
+        website = website_entry.get()
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message=f"No Data File Found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(
+                title=website, message=f"Email: {email}\nPassword: {password}"
+            )
+        else:
+            messagebox.showinfo(title="", message=f"No details for {website} exists.")
+
+
 # UI SETUP
 window = Tk()
 window.title("Password Manager")
@@ -84,30 +103,34 @@ window.config(padx=50, pady=50)
 canvas = Canvas(height=200, width=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.grid(row=0, column=1)
+canvas.grid(row=0, column=1, columnspan=2)
 
 # Labels
 website_label = Label(text="Website:")
-website_label.grid(row=1, column=0)
+website_label.grid(row=1, column=0, sticky=W)
 email_label = Label(text="Email/Username:")
-email_label.grid(row=2, column=0)
-password_label = Label(text="password:")
-password_label.grid(row=3, column=0)
+email_label.grid(row=2, column=0, sticky=W)
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0, sticky=W)
 
 # Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+input_width = 35  # Set a constant width for all entry widgets
+website_entry = Entry(width=input_width)
+website_entry.grid(row=1, column=1, columnspan=2, sticky=W)
 website_entry.focus()
-email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1, columnspan=2)
+email_entry = Entry(width=input_width)
+email_entry.grid(row=2, column=1, columnspan=2, sticky=W)
 email_entry.insert(0, "jordan.mowry@gmail.com")
-password_entry = Entry(width=21)
-password_entry.grid(row=3, column=1)
+password_entry = Entry(width=input_width)
+password_entry.grid(row=3, column=1, sticky=W)
 
 # Buttons
+search_button = Button(text="Search", width=13, command=find_password)
+search_button.grid(row=1, column=2, sticky=E)
 generate_password_button = Button(text="Generate Password", command=generate_password)
-generate_password_button.grid(row=3, column=2)
+generate_password_button.grid(row=3, column=2, sticky=E)
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
+
 
 window.mainloop()
